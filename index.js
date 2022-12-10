52860b46e9608802e8594d5651e87a322d582936
yoshi-non <himekawa.yoshino44@gmail.com>
2022-12-10 19:41:12 +0900

    change: client_url

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const http = require("http")
 const server = http.createServer(app)
 
 const { Server } = require("socket.io");
+
+const CLIENT = process.env.CLIENT_URL
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000"],
+        origin: [CLIENT, "http://localhost:3000"],
         credentials: true,
     },
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT ? process.env.PORT : 5000;
 
 //test
 app.get("/", (req, res) => {
