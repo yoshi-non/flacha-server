a88018f4b04294fd59893232945e8694e4621e5b
yoshi-non <himekawa.yoshino44@gmail.com>
2022-12-10 21:47:48 +0900

    change: https

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const app = express()
 
-const http = require("http")
-const server = http.createServer(app)
+const https = require("https")
+const server = https.createServer(app)
 
 const { Server } = require("socket.io");
 
